1d267357d838af5a907b4e3c76a6f2e0a7ca633f
Herve MARTIN <herve.martin@gmail.com>
2020-02-13 08:36:41 +0100

    Add evil mode to emacs

--- editors/emacs/init.el
+++ editors/emacs/init.el
@@ -1,10 +1,36 @@
-;; Environnement
+;; User Info
+(setq user-full-name "Hervé MARTIN")
+(setq user-mail-address "herve.martin@gmail.com")
+
+;; Install use-package if necessary
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives (append package-archives
+			 '(("melpa" . "http://melpa.org/packages/")
+			 ("marmalade" . "http://marmalade-repo.org/packages/")
+			 ("gnu" . "http://elpa.gnu.org/packages/")
+			 ("org" . "http://orgmode.org/elpa/")
+			 ("elpy" . "http://jorgenschaefer.github.io/packages/"))))
+(package-initialize)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(use-package diminish :ensure t)
+
+;; Enable use-package
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish)                ;; if you use :diminish
+(require 'bind-key)                ;; if you use any :bind variant
+
 (set-language-environment "UTF-8")
 
-(setq custom-file "~/emacs.d/.emacs-custom.el")
-(load custom-file)
-
-;; Affichage
+; (setq custom-file "~/emacs.d/.emacs-custom.el")
+; (load custom-file)
+;; AFFICHAGE
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
@@ -15,3 +41,28 @@
 (setq ring-bell-function 'ignore)
 
 (display-time-mode t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (evil-surround evil diminish use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Enable evil mode
+(require 'evil)
+(evil-mode 1)
+(use-package evil-surround
+  :ensure t
+  :config
+  (global-evil-surround-mode 1))
+
+(use-package evil-leader
+  :ensure t
+  :config
+  (global-evil-leader-mode 1))
