609c17a87ea7223823596edfb66dd5aa4220997f
Herve MARTIN <herve.martin@gmail.com>
2020-02-15 15:33:20 +0100

    Reorganize emacs config

--- editors/emacs/init.el
+++ editors/emacs/init.el
@@ -1,66 +1,18 @@
-;; User Info
-(setq user-full-name "Hervé MARTIN")
-(setq user-mail-address "herve.martin@gmail.com")
-
-;; Install use-package if necessary
+;; Configure package.el to include MELPA.
 (require 'package)
-(setq package-enable-at-startup nil)
-(setq package-archives (append package-archives
-			 '(("melpa" . "http://melpa.org/packages/")
-			 ("marmalade" . "http://marmalade-repo.org/packages/")
-			 ("gnu" . "http://elpa.gnu.org/packages/")
-			 ("org" . "http://orgmode.org/elpa/")
-			 ("elpy" . "http://jorgenschaefer.github.io/packages/"))))
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
 
-;; Bootstrap `use-package'
-(unless (package-installed-p 'use-package)
+;; Ensure that use-package is installed.
+;;
+;; If use-package isn't already installed, it's extremely likely that this is a
+;; fresh installation! So we'll want to update the package repository and
+;; install use-package before loading the literate configuration.
+(when (not (package-installed-p 'use-package))
   (package-refresh-contents)
   (package-install 'use-package))
 
-(use-package diminish :ensure t)
+(org-babel-load-file "~/.emacs.d/configuration.org")
 
-;; Enable use-package
-(eval-when-compile
-  (require 'use-package))
-(require 'diminish)                ;; if you use :diminish
-(require 'bind-key)                ;; if you use any :bind variant
-
-(set-language-environment "UTF-8")
-
-(setq custom-file "~/.emacs.d/custom.el")
-(load custom-file)
-;; AFFICHAGE
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-
-(column-number-mode t)
-
-(setq truncate-partial-width-windows nil)
-(setq ring-bell-function 'ignore)
-
-(display-time-mode t)
-
-; load evil
-(use-package evil
-  :ensure t ;; install the evil package if not installed
-  :init ;; tweak evil's configuration before loading it
-  (setq evil-search-module 'evil-search)
-  (setq evil-ex-complete-emacs-commands nil)
-  (setq evil-vsplit-window-right t)
-  (setq evil-split-window-below t)
-  (setq evil-shift-round nil)
-  (setq evil-want-C-u-scroll t)
-  :config ;; tweak evil after loading it
-  (evil-mode); Enable evil mode
- )
-(use-package evil-surround
-  :ensure t
-  :config
-  (global-evil-surround-mode 1))
-
-(use-package evil-leader
-  :ensure t
-  :config
-  (global-evil-leader-mode 1))
