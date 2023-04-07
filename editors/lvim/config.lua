6515d9b8a7d811ca55f20fc2138ebe01f0197b9d
Herve MARTIN <herve.martin@gmail.com>
2023-04-07 09:19:16 +0200

    Remove minimap and add diffview

--- editors/lvim/config.lua
+++ editors/lvim/config.lua
@@ -220,32 +220,8 @@ lvim.plugins = {
     "rodjek/vim-puppet",
   },
   {
-    "echasnovski/mini.map",
-    branch = "stable",
-    config = function()
-      require('mini.map').setup()
-      local map = require('mini.map')
-      map.setup({
-        integrations = {
-          map.gen_integration.builtin_search(),
-          map.gen_integration.diagnostic({
-            error = 'DiagnosticFloatingError',
-            warn  = 'DiagnosticFloatingWarn',
-            info  = 'DiagnosticFloatingInfo',
-            hint  = 'DiagnosticFloatingHint',
-          }),
-        },
-        symbols = {
-          encode = map.gen_encode_symbols.dot('4x2'),
-        },
-        window = {
-          side = 'right',
-          width = 20, -- set to 1 for a pure scrollbar :)
-          winblend = 15,
-          show_integration_count = false,
-        },
-      })
-    end
+    "sindrets/diffview.nvim",
+    event = "BufRead",
   },
 }
 
