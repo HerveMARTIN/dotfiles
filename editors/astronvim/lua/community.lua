c3629700e69dcb09552d7e6b1d8fd1415b2341cc
Herve MARTIN <herve.martin@gmail.com>
2025-04-03 10:14:17 +0200

    Improve Astro

--- editors/astronvim/lua/community.lua
+++ editors/astronvim/lua/community.lua
@@ -6,6 +6,7 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  --  { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
+  -- { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
+  -- { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
   -- import/override with your plugins folder
 }
