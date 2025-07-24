5db6921ac41aac248effb79539c529a8f52765d2
Herve MARTIN <herve.martin@gmail.com>
2025-07-24 17:19:20 +0200

    Correct mapping and update plugins

--- editors/astronvim/lua/plugins/mappings.lua
+++ editors/astronvim/lua/plugins/mappings.lua
@@ -8,8 +8,6 @@ return {
         n = {
           ["J"] = { "10jzz", desc = "Down 10 lines" },
           ["K"] = { "10kzz", desc = "Up 10 lines" },
-          ["<C-l>"] = { ":bn<CR>", desc = "Buffer on the right" },
-          ["<C-h>"] = { ":bp<CR>", desc = "Buffer on the left" },
           ["<CR>"] = { ":b#<CR>", desc = "Previous buffer" },
           ["<leader>op"] = { ":e Puppetfile<CR>", desc = "Open Puppetfile" },
           ["<leader>oj"] = { ":e Jenkinsfile<CR>", desc = "Open Jenkinsfile" },
@@ -17,10 +15,12 @@ return {
           ["<leader>od"] = { ":e Dockerfile<CR>", desc = "Open Dockerfile" },
           ["<leader>og"] = { ":e Gemfile<CR>", desc = "Open Gemfile" },
           ["<leader>rc"] = { ":AstroReload<CR>", desc = "Reload Astro Config" },
+          ["<C-n>"] = { "<ESC>:bn<CR>", desc = "Buffer on the right" },
+          ["<C-p>"] = { "<ESC>:bp<CR>", desc = "Buffer on the left" },
         },
         i = {
-          ["<C-l>"] = { "<ESC>:bn<CR>", desc = "Buffer on the right" },
-          ["<C-h>"] = { "<ESC>:bp<CR>", desc = "Buffer on the left" },
+          ["<C-n>"] = { "<ESC>:bn<CR>", desc = "Buffer on the right" },
+          ["<C-p>"] = { "<ESC>:bp<CR>", desc = "Buffer on the left" },
         },
       },
     },
